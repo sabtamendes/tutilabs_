730e7451b1eb7444445a780bccd0977d9e953e8e
sabtamendes <alencarsabta@gmail.com>
2024-04-25 20:03:05 -0300

    feat: adding hader

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <AppHeader></AppHeader>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from './components/Header.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader
   }
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+  background-color: #ECECEC;
 }
 </style>
